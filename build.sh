96c36b9738bdc2fd7495b53e69c07213936e8bf4
Daniel Jabbour <daniel@amoe.ba>
2013-09-08 18:53:44 -0700

    Swapping darwin9 for darwin10

--- build.sh
+++ build.sh
@@ -162,6 +162,9 @@ echo "Creating config.h..."
 echo "#define PJ_CONFIG_IPHONE 1 
 #include <pj/config_site_sample.h>" > ${PROJECT_DIR}/pjlib/include/pj/config_site.h
 
+echo "Switching darwin9 to darwin10"
+sed -i 's/darwin9/darwin10/g' ${PROJECT_DIR}/configure-iphone
+
 cd ${PROJECT_DIR}
 
 echo "Building for armv7..."
